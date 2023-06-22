ca90c8bede9d59191aa965f6f5a4068a51280979
ChaoticFS <Magnus.overgaard1711@gmail.com>
2023-06-22 10:05:55 +0200

    Add project files.

---
+++ Entry.cs
@@ -0,0 +1,15 @@
+﻿namespace Shared.Models;
+
+public class Entry
+{
+	public Entry(string title)
+	{
+		Title = title;
+	}
+
+	public int Id { get; set; }
+	public string Title { get; set; }
+	public string Content? { get; set; }
+	public string ImagePath? { get; set; }
+	public int ParentId? { get; set; }
+}
