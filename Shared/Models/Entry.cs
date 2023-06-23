7b6e2eeff5e493442dc4707711838a24032ab98f
Magnus Overgård <magnus.overgaard1711@gmail.com>
2023-06-23 22:51:09 +0200

    Init

---
+++ Shared/Models/Entry.cs
@@ -0,0 +1,22 @@
+﻿namespace Shared.Models;
+
+public class Entry
+{
+    
+	public Entry(int id, string title)
+	{
+		Id = id;
+		Title = title;
+	}
+
+    public Entry(int id, string title, string content)
+    {
+        Id = id;
+        Title = title;
+        Content = content;
+    }
+
+    public int Id { get; set; }
+	public string Title { get; set; }
+	public string? Content { get; set; }
+}
---
+++ Shared/Models/MenuEntry.cs
@@ -0,0 +1,14 @@
+﻿namespace Client.Models;
+internal class MenuEntry
+{
+    MenuEntry(int id, string name, List<MenuEntry>? entries)
+    {
+        Id = id;
+        Name = name;
+        Entries = entries;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<MenuEntry>? Entries { get; set; }
+}
