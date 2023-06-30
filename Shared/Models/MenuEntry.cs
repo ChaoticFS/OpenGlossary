fbc0c1d2309939a77afc775d8411fd5c59b4c603
Magnus Overgård <magnus.overgaard1711@gmail.com>
2023-06-30 07:48:17 +0200

    Added field to allow for dropdown without storing children in memory

--- Shared/Models/MenuEntry.cs
+++ Shared/Models/MenuEntry.cs
@@ -1,14 +1,17 @@
-﻿namespace Client.Models;
-internal class MenuEntry
+﻿namespace Shared.Models;
+
+public class MenuEntry
 {
     MenuEntry(int id, string name, List<MenuEntry>? entries)
     {
         Id = id;
         Name = name;
         Entries = entries;
+        IsOpened = false;
     }
 
     public int Id { get; set; }
     public string Name { get; set; }
     public List<MenuEntry>? Entries { get; set; }
+    public bool IsOpened { get; set; }
 }
